074199ccc69703d0a2bbbe74cbb0837b1dbffc3c
Jouni Linden <jouni.linden@gmail.com>
2015-01-17 19:30:56 +0200

    prod env to prod start script

--- start-prod.sh
+++ start-prod.sh
@@ -3,5 +3,5 @@ git pull
 go build
 killall rssFetcher
 sleep 1
-nohup ./rssFetcher -address localhost:28517 > rssFetcher.log 2>&1&
+nohup ./rssFetcher -env prod -address localhost:28517 > rssFetcher.log 2>&1&
 ps aux|grep rssFetcher
